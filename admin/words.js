b99d5a34aa029743668d4712e2449429478017a0
Jens Wolf <jens.wolf2@gmail.com>
2021-04-21 20:48:49 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'react-dashboard adapter settings': {
+		'en': 'Adapter settings for react-dashboard',
+		'de': 'Adaptereinstellungen für react-dashboard',
+		'ru': 'Настройки адаптера для react-dashboard',
+		'pt': 'Configurações do adaptador para react-dashboard',
+		'nl': 'Adapterinstellingen voor react-dashboard',
+		'fr': "Paramètres d'adaptateur pour react-dashboard",
+		'it': "Impostazioni dell'adattatore per react-dashboard",
+		'es': 'Ajustes del adaptador para react-dashboard',
+		'pl': 'Ustawienia adaptera dla react-dashboard',
+		'zh-cn': 'react-dashboard的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
